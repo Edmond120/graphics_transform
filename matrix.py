1eebdf6bdb9e6559d6a67be1eca11b1f4892168e
edmond120 <edmondwong888@msn.com>
2018-03-05 00:44:18 -0500

    a

---
+++ matrix.py
@@ -0,0 +1,67 @@
+import math
+
+def make_translate( x, y, z ):
+	m = ident(4)
+	c = len(m[0]) - 1
+	m[0][c] = x
+	m[1][c] = y
+	m[2][c] = z
+	return m
+
+def make_scale( x, y, z ):
+	m = new_matrix(rows,cols)
+	m[0][0] = x
+	m[1][1] = y
+	m[2][2] = z
+	m[3][3] = 1
+	return m
+
+def make_rotX( theta ):    
+    pass
+
+def make_rotY( theta ):
+    pass
+
+def make_rotZ( theta ):
+    pass
+
+def print_matrix( matrix ):
+	rows = len(matrix)
+	cols = len(matrix[0])
+	for row in xrange(rows):
+		print '[ ',
+		for col in xrange(cols):
+			s = str(matrix[row][col])
+			print ' ' * (3 - len(s)) + s + ' ',
+		print ']'
+
+def ident( r ):
+	l = r
+	m = new_matrix(rows=l,cols=l)
+	for i in xrange( l ):
+		m[i][i] = 1
+	return m
+
+#m1 * m2 -> m2
+def matrix_mult( m1, m2 ):
+	if(len(m1[0]) == len(m2)):
+		rows = len(m1)
+		cols = len(m2[0])
+		m = new_matrix(rows,cols)
+		for row in xrange(len(m1)):
+			m2c = xrange(len(m2[0]))
+			for i in xrange(len(m1[0])):
+				for col in m2c:
+					m[row][col] += m1[row][i] * m2[i][col]
+		return m
+	else:
+		return None
+
+
+def new_matrix(rows = 4, cols = 4):
+    m = []
+    for r in xrange( rows ):
+        m.append( [] )
+        for c in xrange( cols ):
+            m[r].append( 0 )
+    return m
